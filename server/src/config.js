6ee370e13bef180e139657a06dc914caca8ac379
unknown <rohit995379@gmail.com>
2025-09-28 03:43:27 +0530

    mongoDb uri update

--- server/src/config.js
+++ server/src/config.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 module.exports = {
   port: process.env.PORT || 4000,
-  // mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/',
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || 'dev_secret',
   corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
