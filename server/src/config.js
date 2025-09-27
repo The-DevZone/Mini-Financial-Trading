f3e6c8fd9e0369c825ccb1f812e469cd14c924d9
unknown <rohit995379@gmail.com>
2025-09-28 03:29:05 +0530

    fix: added start script for production

--- server/src/config.js
+++ server/src/config.js
@@ -3,9 +3,13 @@ dotenv.config();
 
 module.exports = {
   port: process.env.PORT || 4000,
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/minitrader',
+  // mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || 'dev_secret',
   corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   uploadDir: process.env.UPLOAD_DIR || './uploads',
   // redisUrl: process.env.REDIS_URL ||  'redis://127.0.0.1:6379',
+
+  
 };
+
