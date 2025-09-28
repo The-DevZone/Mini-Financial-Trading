3f1d4e2a354285d8c6bfd503cf484d9acf9b0b8f
unknown <rohit995379@gmail.com>
2025-09-28 11:23:51 +0530

    commit

--- server/src/config.js
+++ server/src/config.js
@@ -2,11 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  port: process.env.PORT || 4000,
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  jwtSecret: process.env.JWT_SECRET || 'dev_secret',
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  uploadDir: process.env.UPLOAD_DIR || './uploads',
+  port: process.env.port || 4000,
+  mongoUrl: process.env.mongoUrl || 'mongodb+srv://rohit99539953_db_user:U3xVYDKpZgapfpJD@cluster0.e8okq6c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  jwtSecret: process.env.jwtSecret || 'dev_secret',
+  corsOrigin: process.env.corsOrigin || 'http://localhost:5173',
+  uploadDir: process.env.uploadDir || './uploads',
   // redisUrl: process.env.REDIS_URL ||  'redis://127.0.0.1:6379',
 
   
